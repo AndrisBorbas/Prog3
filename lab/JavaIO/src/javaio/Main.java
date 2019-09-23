defe4948865646ec80cb3ebb1595e5772c2cfc3c
AndrisBorbas <andris.borbas@gmail.com>
2019-09-23 22:59:19 +0200

    l3f4 done

--- lab/JavaIO/src/javaio/Main.java
+++ lab/JavaIO/src/javaio/Main.java
@@ -2,8 +2,11 @@ package javaio;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
 
 public class Main {
 
@@ -32,6 +35,15 @@ public class Main {
 
 			if (cmd[0].equals("reclist") || cmd[0].equals("lsdir"))
 				reclist(f, "");
+
+			if (cmd[0].equals("length"))
+				length(cmd);
+
+			if (cmd[0].equals("tail"))
+				tail(cmd);
+
+			if (cmd[0].equals("grep"))
+				grep(cmd);
 		}
 
 	}
@@ -94,5 +106,80 @@ public class Main {
 				reclist(i, tab + "  ");
 			}
 		}
+	}
+
+	protected static void length(String[] args) {
+		if (args.length == 2) {
+			File[] files = f.listFiles();
+			for (File i : files) {
+				if (i.getName().equals(args[1])) {
+					System.out.println(i.length() + " bytes");
+					return;
+				}
+			}
+		}
+		System.out.println("File not found!");
+	}
+
+	protected static void tail(String[] args) throws FileNotFoundException, IOException {
+		int n = 0;
+		File file = null;
+		if (args.length == 4) {
+			if (args[1].equals("-n")) {
+				n = Integer.parseInt(args[2]);
+				file = new File(f.getCanonicalPath() + "/" + args[3]);
+			}
+
+		}
+		if (args.length == 2) {
+			n = 10;
+			file = new File(f.getCanonicalPath() + "/" + args[1]);
+		}
+		if (file == null || n <= 0) {
+			System.out.println("Wrong command!");
+			return;
+		}
+		if (!file.exists()) {
+			System.out.println("File doesn't exist!");
+			return;
+		}
+
+		LinkedList<String> list = new LinkedList<String>();
+		BufferedReader br = new BufferedReader(new FileReader(file.getCanonicalPath()));
+		String temp = br.readLine();
+		while (temp != null) {
+			if (list.size() == n) {
+				list.removeFirst();
+			}
+			list.add(temp);
+			temp = br.readLine();
+		}
+		for (String i : list) {
+			System.out.println(i);
+		}
+		br.close();
+	}
+
+	protected static void grep(String[] args) throws IOException {
+		if (args.length != 3) {
+			System.out.println("Wrong command!");
+			return;
+		}
+		File file = null;
+		file = new File(f.getCanonicalPath() + "/" + args[2]);
+		if (!file.exists()) {
+			System.out.println("File doesn't exist!");
+			return;
+		}
+		
+		String pattern = args[1];
+		BufferedReader br = new BufferedReader(new FileReader(file.getCanonicalPath()));
+		String line;
+		while ((line = br.readLine()) != null) {
+			if (line.matches("(.*)" + pattern + "(.*)")) {
+				System.out.println(line);
+			}
+		}
+		br.close();
 	}
 }
